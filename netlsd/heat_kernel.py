c99ba1cf5ac731b33faabafe64a267330b187e9e
Luke Welton <luke.welton97@gmail.com>
2018-11-23 15:49:17 -0600

    implement wave kernel & complete normalization for heat

--- netlsd/heat_kernel.py
+++ netlsd/heat_kernel.py
@@ -1,28 +1,32 @@
 import numpy as np
 
 #--------PARAMS----------#
-#eigvals: matrix of eigenvalues of the (normalized) laplacian of the graph
-#timespaces: timescales for diffusion sampling
-#normalization: determines which graph to normalize the result with 
+# eigvals: matrix of eigenvalues of the (normalized) laplacian of the graph
+# timespaces: timescales for diffusion sampling
+# normalization: determines which graph to normalize the result with
 # {"none": no normalization, "empty": normalization with empty graph, "complete": normaliization w/ complete graph}
 
 #-------RETURNS----------#
-#NetLSD spectral descriptor for chosen graph (numpy.ndarray) given by heat kernel trace
+# NetLSD spectral descriptor for chosen graph (numpy.ndarray) given by heat kernel trace
 
-def heat(eigenvals, timespaces, normalization):
+
+def heat(eigenvals, timespaces, normalization, normalized_laplacian):
 	n_vertices = eigenvals.shape[0]
 	heat_kernel_trace = []
 	for i in range(len(timespaces)):
-			#computes a matrix given by:
-			#(heat_kernel)(i,j) = amount of heat transferred from vertex i to vertex j in time t
+			# computes a matrix given by:
+			# (heat_kernel)(i,j) = amount of heat transferred from vertex i to vertex j in time t
 			heat_kernel = np.sum(np.exp(-timespaces[i] * eigenvals))
-			#add heat kernel to trace representation
+
+			# add heat kernel to trace representation
 			heat_kernel_trace.append(heat_kernel)
 
-	if normalization == "empty": #normalize heat kernel trace against empty graph
+	if normalization == "empty":  # normalize heat kernel trace against empty graph
 		return heat_kernel_trace / n_vertices
-	#TODO compute complete normalization
-	#else if normalization == "complete": #normalize heat kernel trace against complete graph
-	elif normalization == "none" or normalization == None: #return heat kernel trace without normalization
+	elif normalization == "complete" and normalized_laplacian:  # normalize heat kernel trace against complete graph
+		return heat_kernel_trace / (1 + (n_vertices - 1) * np.exp(-timespaces))
+	elif normalization == "complete":
+		return heat_kernel_trace / (1 + n_vertices * np.exp(-n_vertices * timespaces))
+	else:  # return heat kernel trace without normalization
 		return heat_kernel_trace
 
--- netlsd/wave_kernel.py
+++ netlsd/wave_kernel.py
@@ -1,15 +1,27 @@
 import numpy as np
 
 #--------PARAMS----------#
-#eigvals: matrix of eigenvalues of the (normalized) laplacian of the graph
-#timespaces: timescales for diffusion sampling
-#normalization: determines which graph to normalize the result with 
-# {"none": no normalization, "empty": normalization with empty graph, "complete": normaliization w/ complete graph}
+# eigvals: matrix of eigenvalues of the (normalized) laplacian of the graph
+# timespaces: timescales for diffusion sampling
+# normalization: determines which graph to normalize the result with
+# {"none": no normalization, "empty": normalization with empty graph, "complete": normalization w/ complete graph}
 
 #-------RETURNS----------#
-#NetLSD spectral descriptor for chosen graph (numpy.ndarray) given by wave kernel trace at specified time scales
-#TODO compute wave kernel
-def wave(eigvals, timespaces, normalization):
+# NetLSD spectral descriptor for chosen graph (numpy.ndarray) given by wave kernel trace at specified time scales
 
-	return 0
 
+def wave(eigenvals, timespaces, normalization, normalized_laplacian):
+	n_values = eigenvals.shape[0]
+	wave_kernel_trace = []
+	for i in range(len(timespaces)):
+		wave_kernel = np.sum(np.exp(-1j * timespaces[i] * eigenvals))
+		wave_kernel_trace.append(wave_kernel)
+
+	if normalization == "empty":
+		return wave_kernel_trace / n_values
+	elif normalization == "complete" and normalized_laplacian:
+		return wave_kernel_trace / (1 + (n_values - 1) * np.cos(timespaces))
+	elif normalization == "complete":
+		return wave_kernel_trace / (1 + (n_values - 1) * np.cos(n_values * timespaces))
+	else:
+		return wave_kernel_trace
