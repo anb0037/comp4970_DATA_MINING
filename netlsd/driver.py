095abd12b01d17e2a370cfb930c44b9aae21a417
Luke Welton <luke.welton97@gmail.com>
2018-12-02 18:03:24 -0600

    add comparison stuff

--- netlsd/driver.py
+++ netlsd/driver.py
@@ -2,18 +2,47 @@ import netlsd as nlsd
 import extract_data as ed
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 # construct graph data from dataset
 # param is selected data set prefix
 
-if __name__ == "__main__":
+DATASETS = {"DD", "MUTAG", "NCI1", "NCI109", "PROTEINS",
+            "IMDB-BINARY", "IMDB-MULTI", "REDDIT-BINARY"}
+KERNELS = {"heat", "wave"}
+NORMALIZATIONS = ("none", "empty", "complete")
+
+
+class DatasetAnalytics:
+    def __init__(self, name, kernel, normalization):
+        self.name = name
+        self.kernel = kernel
+        self.normalization = normalization
+
+        self.accuracy = 0
+        self.precision = 0
+        self.recall = 0
+        self.fscore = 0
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def calculate_fscore(self):
+        self.fscore = 2 * ((self.precision * self.recall) / (self.precision + self.recall))
+
+    def __str__(self):
+        return ",".join([self.name, self.kernel, self.normalization,
+                         str(self.accuracy), str(self.precision), str(self.recall), str(self.fscore)])
+
+
+def run(dataset):
     # params
-    dataset_prefix = 'MUTAG' # vary this
-    kernel_type = 'heat' # vary this
-    normalization = 'empty' # vary this
+    dataset_prefix = dataset.name
+    kernel_type = dataset.kernel
+    normalization = dataset.normalization
     normalized_laplacian = True # keep this fixed
     timespaces = np.logspace(-2, 2, 250) # keep this fixed
     training_ratio = 0.8 # keep this fixed
-    classification_difficulty = 0.5 #keep this fixed
+    classification_difficulty = 0.5 # keep this fixed
 
     if len(sys.argv) >= 2 and isinstance(sys.argv[1], str):
         dataset_prefix = sys.argv[1]
@@ -30,7 +59,7 @@ if __name__ == "__main__":
     testing_set = complete_data[bound:]
 
     print("Calculating descriptors...")
-    #calculate descriptors for test data
+    # calculate descriptors for test data
     test_descriptors = []
     for graph in testing_set:
         descriptor = nlsd.netlsd(graph[0], kernel=kernel_type, timespaces=timespaces, normalized_laplacian=normalized_laplacian, normalization=normalization)
@@ -44,7 +73,7 @@ if __name__ == "__main__":
     print("Predicting test data...")
     # construct an array of (bool, bool) tuples for each graph in the test data given by (prediction, actual)
     # True if the graph is authentic, False if it is not
-    predictions = [] 
+    predictions = []
     for index, graph in enumerate(testing_set):
 		# predict the label of the test graph as the label of it's first nearest neighbor in the training set
         first_nn_index = nlsd.first_NN(test_descriptors[index], training_descriptors)
@@ -65,8 +94,66 @@ if __name__ == "__main__":
                 fn += 1
             else:
                 tn += 1
-    accuracy = (tp + tn) / (tp + tn + fp + fn)
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    fscore = 2 * ((precision * recall) / (precision + recall))
-    print("Complete")
+
+    dataset.accuracy = (tp + tn) / (tp + tn + fp + fn)
+    dataset.precision = tp / (tp + fp)
+    dataset.recall = tp / (tp + fn)
+    dataset.calculate_fscore()
+
+
+if __name__ == "__main__":
+    all_analytics = []
+
+    with open("output.txt", "w") as file:
+        for dataset in {'MUTAG'}:
+            for kernel in KERNELS:
+                for n in NORMALIZATIONS:
+                    da = DatasetAnalytics(dataset, kernel, n)
+                    run(da)
+                    all_analytics.append(da)
+                    file.write(str(da))
+
+    fig, ax = plt.subplots()
+
+    names = []
+    for a in all_analytics:
+        if a.name not in names:
+            if '-' in a.name:
+                names.append('\n'.join(a.name.split('-')))
+            else:
+                names.append(a.name)
+
+    fscores = {
+        "heat_none": np.zeros(len(names)),
+        "wave_none": np.zeros(len(names)),
+        "heat_empty": np.zeros(len(names)),
+        "wave_empty": np.zeros(len(names)),
+        "heat_complete": np.zeros(len(names)),
+        "wave_complete": np.zeros(len(names))
+    }
+
+    index = 0
+    for a in all_analytics:
+        key = "_".join([a.kernel, a.normalization])
+
+        if fscores[key][index] > 0:
+            index += 1
+        fscores[key][index] = a.fscore
+
+    y_pos = np.arange(len(names))
+    width = 0.15
+
+    for i, key in enumerate(fscores):
+        ax.bar(y_pos + width * (2 * i - 5)/2, fscores[key], width, label=", ".join(key.split("_")))
+
+    ax.set_xticks(y_pos)
+    ax.set_xticklabels(names)
+    ax.set_ylabel("Dataset Fscore")
+    ax.set_title("Dataset Fscore Comparison")
+    ax.legend(loc='lower center')
+
+    plt.show()
+
+
+
+
