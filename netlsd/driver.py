f145400e5bd46b36f2e1fe2be0724e2c23c6ff3b
anb0037 <anb0037@auburn.edu>
2018-12-10 19:02:59 -0600

    added more timestamps, isolated preprocessing step

--- netlsd/driver.py
+++ netlsd/driver.py
@@ -3,19 +3,26 @@ import extract_data as ed
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+import time
 
-DATASETS = {"DD", "MUTAG", "NCI1", "NCI109", "PROTEINS",
-            "IMDB-BINARY", "IMDB-MULTI", "REDDIT-BINARY"}
+#DATASETS = {"DD", "MUTAG", "NCI1", "NCI109", "PROTEINS",
+#            "IMDB-BINARY", "IMDB-MULTI", "REDDIT-BINARY"}
+DATASETS = {"MUTAG", "PROTEINS"}
 KERNELS = {"heat", "wave"}
 NORMALIZATIONS = ("none", "empty", "complete")
+normalized_laplacian = True # keep this fixed
+timespaces = np.logspace(-2, 2, 250) # keep this fixed
+training_ratio = 0.8 # keep this fixed
+classification_difficulty = 0.5 # keep this fixed
 
 
 class DatasetAnalytics:
-    def __init__(self, name, kernel, normalization):
+    def __init__(self, name, kernel, normalization, training_set, testing_set):
         self.name = name
         self.kernel = kernel
         self.normalization = normalization
-
+        self.training_set = training_set
+        self.testing_set = testing_set
         self.accuracy = 0
         self.precision = 0
         self.recall = 0
@@ -37,33 +44,19 @@ def run(dataset):
     dataset_prefix = dataset.name
     kernel_type = dataset.kernel
     normalization = dataset.normalization
-    normalized_laplacian = True # keep this fixed
-    timespaces = np.logspace(-2, 2, 250) # keep this fixed
-    training_ratio = 0.8 # keep this fixed
-    classification_difficulty = 0.5 # keep this fixed
     
     print('Executing {0} kernel with {1} normalization on {2} dataset'.format(dataset.kernel, dataset.normalization, dataset.name))
-	start = time.time()
-    # extract real data from csv
-    graphs = ed.extract_data(dataset_prefix)
-    # generate fake data
-    fake_graphs = ed.shuffle_graphs(graphs, classification_difficulty)
-    # combining & shuffling real and fake data
-    complete_data = ed.combine(graphs, fake_graphs)
-    # splitting training & testing sets
-    bound = int(len(complete_data) * training_ratio)
-    training_set = complete_data[:bound]
-    testing_set = complete_data[bound:]
+    start = time.time()
 
     print("Calculating descriptors...")
     # calculate descriptors for test data
     test_descriptors = []
-    for graph in testing_set:
+    for graph in dataset.testing_set:
         descriptor = nlsd.netlsd(graph[0], kernel=kernel_type, timespaces=timespaces, normalized_laplacian=normalized_laplacian, normalization=normalization)
         test_descriptors.append(descriptor)
     # calculate descriptors for training data
     training_descriptors = []
-    for graph in training_set:
+    for graph in dataset.training_set:
         descriptor = nlsd.netlsd(graph[0], kernel=kernel_type, timespaces=timespaces, normalized_laplacian=normalized_laplacian, normalization=normalization)
         training_descriptors.append(descriptor)
 
@@ -72,7 +65,7 @@ def run(dataset):
     # True if the graph is authentic, False if it is not
     predictions = []
     for index, graph in enumerate(testing_set):
-		# predict the label of the test graph as the label of it's first nearest neighbor in the training set
+        # predict the label of the test graph as the label of it's first nearest neighbor in the training set
         first_nn_index = nlsd.first_NN(test_descriptors[index], training_descriptors)
         prediction = training_set[first_nn_index][1]
         predictions.append((prediction, testing_set[index][1]))
@@ -96,9 +89,9 @@ def run(dataset):
     dataset.precision = tp / (tp + fp)
     dataset.recall = tp / (tp + fn)
     dataset.calculate_fscore()
-	end = time.time()
-	print('Completed in {0:.2f} seconds'.format(end - start))
-	
+    end = time.time()
+    print('Completed in {0:.2f} seconds'.format(end - start))
+    
 
 if __name__ == "__main__":
     all_analytics = []
@@ -106,9 +99,23 @@ if __name__ == "__main__":
     with open("output.csv", "w") as file:
         file.write(",".join(["dataset", "kernel", "normalization", "accuracy", "precision", "recall", "fscore"]) + "\n")
         for dataset in DATASETS:
+            print("Preprocessing data from {0}".format(dataset))
+            start = time.time()
+            # extract real data from csv
+            graphs = ed.extract_data(dataset)
+            # generate fake data
+            fake_graphs = ed.shuffle_graphs(graphs, classification_difficulty)
+            # combining & shuffling real and fake data
+            complete_data = ed.combine(graphs, fake_graphs)
+            # splitting training & testing sets
+            bound = int(len(complete_data) * training_ratio)
+            training_set = complete_data[:bound]
+            testing_set = complete_data[bound:]
+            end = time.time()
+            print("Preprocessing complete in {0:.2f} seconds".format(end-start))
             for kernel in KERNELS:
                 for n in NORMALIZATIONS:
-                    da = DatasetAnalytics(dataset, kernel, n)
+                    da = DatasetAnalytics(dataset, kernel, n, training_set, testing_set)
                     run(da)
                     all_analytics.append(da)
                     file.write(str(da) + "\n")
